d6e85e7a7a444a6f42f96d9927c3d40f044adc0f
Ratchakij <ratchakijs@gmail.com>
2022-11-23 18:28:28 +0700

    Finding Longest Common Prefix

---
+++ Exam_1.js
@@ -0,0 +1,17 @@
+let strs = ["flower", "flow", "flight"];
+var longestCommonPrefix = function (strs) {
+  for (let i = 0; i < strs.length; i++) {
+    for (let j = 0; j < strs[i].length; j++) {
+      while (strs[i][j] == strs[i + 1][j] && strs[i][j] == strs[i + 2][j]) {
+        j++;
+      }
+      if (!i && !j) {
+        return console.log("");
+      } else {
+        return console.log(String(strs).substr(0, j));
+      }
+    }
+  }
+};
+
+longestCommonPrefix(strs);
